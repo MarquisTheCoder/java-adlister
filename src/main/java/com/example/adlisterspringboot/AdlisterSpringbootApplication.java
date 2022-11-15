290fa306b09561c359bd38d2138d8a3609ad55b4
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-11-15 11:38:26 -0600

    autosave script

---
+++ src/main/java/com/example/adlisterspringboot/AdlisterSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.adlisterspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdlisterSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdlisterSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adlisterspringboot/AdlisterSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adlisterspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdlisterSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
